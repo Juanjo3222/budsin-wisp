98ef792855d96141f2af662e08ec569e94cb2103
Juanjo3222 <86850741+Juanjo3222@users.noreply.github.com>
2026-04-28 20:10:00 -0500

    Refactor WispServer import syntax

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { createServer } from "http";
-import { WispServer } from "wisp-server-node";
-
+import pkg from "wisp-server-node";
+const { WispServer } = pkg;
 const app = express();
 const PORT = process.env.PORT || 8080;
 
